b30dd3fa68b8302ff50b1070807416a91f2afbee
Ben Jefferies <benjefferies@echosoft.uk>
2020-02-14 15:14:53 +0000

    Remove markup key

--- run.py
+++ run.py
@@ -43,7 +43,7 @@ def comment_on_pr():
         print("PR_COMMENT must be set")
         exit(1)
 
-    content = {'content': {'raw': 'Automated PR comment\n\n\x1b[0m\x1b[1mRefreshing Terraform state in-memory prior to plan...\x1b[0m\nThe refreshed state will be used to calculate this plan, but will not be\npersisted to local or remote state storage.\n\x1b[0m\n\x1b[0m\x1b[1mmodule.terraform_state.aws_dynamodb_table.terraform_locks: Refreshing state... [id=terraform-locks]\x1b[0m\n\x1b[0m\x1b[1mmodule.terraform_state.aws_s3_bucket.terraform_state: Refreshing state... [id=terraform-state-071021110864]\x1b[0m\n\n------------------------------------------------------------------------\n\n\x1b[0m\x1b[1m\x1b[32mNo changes. Infrastructure is up-to-date.\x1b[0m\x1b[32m\n\nThis means that Terraform did not detect any differences between your\nconfiguration and real physical resources that exist. As a result, no\nactions need to be performed.\x1b[0m\nReleasing state lock. This may take a few moments...', 'markup': 'markdown'}}
+    content = {'content': {'raw': f"Automated PR comment\n\n```\n{comment}\n```"}}
     url = f"{bitbucket_api}/2.0/repositories/{owner}/{repo_slug}/pullrequests/{pr_id}/comments"
     print(f"Adding comment: {url}")
     print(f"{content}")
